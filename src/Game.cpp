0c711b817f9682359b4bcc9f59e192a76eac658b
Juho Roivas <juho-petteri.1.roivas2@edu.karelia.fi>
2014-04-13 04:02:22 +0300

    A slight tweak. I messed the resolution up.

--- src/Game.cpp
+++ src/Game.cpp
@@ -67,7 +67,7 @@ void Game::Play()
 	std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
   }
   //Creates the window for the splash screen
-  SDL_Window *win = SDL_CreateWindow("Quickescape", 100, 100, 640, 480,
+  SDL_Window *win = SDL_CreateWindow("Quickescape", 100, 100, 640, 400,
 	SDL_WINDOW_SHOWN);
   if (win == nullptr){
 	std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
