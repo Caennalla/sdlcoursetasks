86c56e249ec4b796ce7c27bfa17208a4460696a8
Juho Roivas <juho-petteri.1.roivas2@edu.karelia.fi>
2014-05-07 15:02:21 +0300

    Added the build too

---
+++ Builds/CMakeFiles/quickescape.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Kouluhommia/C++/SDL/Project/src/Commands.cpp" "D:/Kouluhommia/C++/SDL/Builds/CMakeFiles/quickescape.dir/src/Commands.cpp.obj"
+  "D:/Kouluhommia/C++/SDL/Project/src/Game.cpp" "D:/Kouluhommia/C++/SDL/Builds/CMakeFiles/quickescape.dir/src/Game.cpp.obj"
+  "D:/Kouluhommia/C++/SDL/Project/src/GameObject.cpp" "D:/Kouluhommia/C++/SDL/Builds/CMakeFiles/quickescape.dir/src/GameObject.cpp.obj"
+  "D:/Kouluhommia/C++/SDL/Project/src/GameScene.cpp" "D:/Kouluhommia/C++/SDL/Builds/CMakeFiles/quickescape.dir/src/GameScene.cpp.obj"
+  "D:/Kouluhommia/C++/SDL/Project/src/GuardRoom.cpp" "D:/Kouluhommia/C++/SDL/Builds/CMakeFiles/quickescape.dir/src/GuardRoom.cpp.obj"
+  "D:/Kouluhommia/C++/SDL/Project/src/IntroScene.cpp" "D:/Kouluhommia/C++/SDL/Builds/CMakeFiles/quickescape.dir/src/IntroScene.cpp.obj"
+  "D:/Kouluhommia/C++/SDL/Project/src/Inventory.cpp" "D:/Kouluhommia/C++/SDL/Builds/CMakeFiles/quickescape.dir/src/Inventory.cpp.obj"
+  "D:/Kouluhommia/C++/SDL/Project/src/Logger.cpp" "D:/Kouluhommia/C++/SDL/Builds/CMakeFiles/quickescape.dir/src/Logger.cpp.obj"
+  "D:/Kouluhommia/C++/SDL/Project/src/Page.cpp" "D:/Kouluhommia/C++/SDL/Builds/CMakeFiles/quickescape.dir/src/Page.cpp.obj"
+  "D:/Kouluhommia/C++/SDL/Project/src/Player.cpp" "D:/Kouluhommia/C++/SDL/Builds/CMakeFiles/quickescape.dir/src/Player.cpp.obj"
+  "D:/Kouluhommia/C++/SDL/Project/src/PropertyHolder.cpp" "D:/Kouluhommia/C++/SDL/Builds/CMakeFiles/quickescape.dir/src/PropertyHolder.cpp.obj"
+  "D:/Kouluhommia/C++/SDL/Project/src/Room.cpp" "D:/Kouluhommia/C++/SDL/Builds/CMakeFiles/quickescape.dir/src/Room.cpp.obj"
+  "D:/Kouluhommia/C++/SDL/Project/src/RoomFactory.cpp" "D:/Kouluhommia/C++/SDL/Builds/CMakeFiles/quickescape.dir/src/RoomFactory.cpp.obj"
+  "D:/Kouluhommia/C++/SDL/Project/src/SDLApp.cpp" "D:/Kouluhommia/C++/SDL/Builds/CMakeFiles/quickescape.dir/src/SDLApp.cpp.obj"
+  "D:/Kouluhommia/C++/SDL/Project/src/Scene.cpp" "D:/Kouluhommia/C++/SDL/Builds/CMakeFiles/quickescape.dir/src/Scene.cpp.obj"
+  "D:/Kouluhommia/C++/SDL/Project/src/Stairway.cpp" "D:/Kouluhommia/C++/SDL/Builds/CMakeFiles/quickescape.dir/src/Stairway.cpp.obj"
+  "D:/Kouluhommia/C++/SDL/Project/src/main.cpp" "D:/Kouluhommia/C++/SDL/Builds/CMakeFiles/quickescape.dir/src/main.cpp.obj"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "D:/Kouluhommia/C++/SDL/Project/src"
+  "D:/Kouluhommia/C++/SDL/Project/quickescape"
+  "D:/Kouluhommia/C++/SDL/Project/deps/include"
+  "D:/Kouluhommia/programs/boost_1_55_0"
+  "D:/Kouluhommia/C++/SDL/Project/deps/include/SDL2"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ Builds/CMakeFiles/quickescape.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/quickescape.dir/src/Game.cpp.obj"
+  "CMakeFiles/quickescape.dir/src/Room.cpp.obj"
+  "CMakeFiles/quickescape.dir/src/RoomFactory.cpp.obj"
+  "CMakeFiles/quickescape.dir/src/GuardRoom.cpp.obj"
+  "CMakeFiles/quickescape.dir/src/Stairway.cpp.obj"
+  "CMakeFiles/quickescape.dir/src/GameObject.cpp.obj"
+  "CMakeFiles/quickescape.dir/src/PropertyHolder.cpp.obj"
+  "CMakeFiles/quickescape.dir/src/Player.cpp.obj"
+  "CMakeFiles/quickescape.dir/src/Inventory.cpp.obj"
+  "CMakeFiles/quickescape.dir/src/main.cpp.obj"
+  "CMakeFiles/quickescape.dir/src/Logger.cpp.obj"
+  "CMakeFiles/quickescape.dir/src/Commands.cpp.obj"
+  "CMakeFiles/quickescape.dir/src/SDLApp.cpp.obj"
+  "CMakeFiles/quickescape.dir/src/Page.cpp.obj"
+  "CMakeFiles/quickescape.dir/src/Scene.cpp.obj"
+  "CMakeFiles/quickescape.dir/src/IntroScene.cpp.obj"
+  "CMakeFiles/quickescape.dir/src/GameScene.cpp.obj"
+  "quickescape.pdb"
+  "quickescape.exe"
+  "quickescape.exe.manifest"
+  "libquickescape.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/quickescape.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
